14af37a35aff1462c8c06a8ba1c9971f81cecbf7
Corsimy <1799281482@qq.com>
2024-04-03 15:19:28 +0800

    test in other for connection

--- src/components/other.tsx
+++ src/components/other.tsx
@@ -1,3 +1,32 @@
+// export default function Other() {
+//   return <main>TODO: 修改显示的内容 其他</main>;
+// }
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function Other() {
-  return <main>TODO: 修改显示的内容 其他</main>;
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/other/');
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error('There was a problem fetching the data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      {message ? (
+        <p>{message}</p>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </main>
+  );
 }
